ee8d4e852a9b9ee7c89c189be78f58b01273a194
Mazin Zakaria <mazinzakaria1@gmail.com>
2015-04-26 22:36:51 -0400

    changes

--- server.cpp
+++ server.cpp
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 
     if (argc != 3) {
 	cout << "usage: " << argv[0] << " <port> <secret key>" << endl;
-	exit(0);
+	exit(-1);
     }
     port = atoi(argv[1]);
     secretKey = atoi(argv[2]);
@@ -59,14 +59,15 @@ int main(int argc, char **argv)
         Rio_readinitb(&rio, connfd);
         
         if(isValidKey(&rio, secretKey)) {     
-            //Read what request is given       
+            // Read what request is given (next 4 bytes after the secret key)    
             char requestbuf[4];
             unsigned int networkOrder;
             if(Rio_readnb(&rio, requestbuf, 4) == 4) {
                 memcpy(&networkOrder, &requestbuf, 4);
                 requestType = ntohl(networkOrder);
             }
-
+            ////
+            // Call the appropriate function 
             if(requestType == 0) {
                 cout << "Request Type     = get" << endl;
                 status = mcget(&rio, connfd);
@@ -87,27 +88,32 @@ int main(int argc, char **argv)
             }
         }
 
-        if(status == 0) { 
+        if(status == 0) 
+        { 
             cout << "Operation Status = success" << endl;
         }
-        else { 
+        else 
+        { 
             cout << "Operation Status = error" << endl; 
-            }
+        }
         cout << "------------------------------------" << endl;
-	Close(connfd);
+	
+    Close(connfd);
     }
     exit(0);
 }
 
+// Checks the given key against the actual secret key
 bool isValidKey(rio_t *rio, unsigned int secretKey) {
     char buf[4];
     unsigned int givenKey;
     unsigned int networkOrder;
-
+    // Read the first 4 bytes which should be the secret key
+    // according to the specification.
     if(Rio_readnb(rio, buf, 4) == 4) {
         memcpy(&networkOrder, &buf, 4);
         givenKey = ntohl(networkOrder);        
-
+        
         cout << "Secret Key       = " << givenKey << endl;
         if(givenKey == secretKey) { 
             return true; 
@@ -121,16 +127,18 @@ bool isValidKey(rio_t *rio, unsigned int secretKey) {
 
 
 int mcput(rio_t *rio, int connfd) {
-    size_t n;
+    FILE *myfile;
     char fileNameBuf[80];
     char fileName[80];
     char fileSizeBuf[4];
     unsigned int fileSize, networkOrder, status, messageSize;
     char dataBuf[MAX_FILE_SIZE];
-    char *data, *message;
-    FILE *file;
+    char *data;
+    
+    
+    char *response;
 
-    if((n = Rio_readnb(rio, fileNameBuf, 80)) == 80) {
+    if(Rio_readnb(rio, fileNameBuf, 80) == 80) {
         memcpy(&fileName, &fileNameBuf, 80);
         cout << "Filename         = " << fileName << endl;
     } else {
@@ -138,14 +146,14 @@ int mcput(rio_t *rio, int connfd) {
         status = -1;
     }
 
-    if((n = Rio_readnb(rio, fileSizeBuf, 4)) == 4) {
+    if(Rio_readnb(rio, fileSizeBuf, 4) == 4) {
         memcpy(&networkOrder, &fileSizeBuf, 4);
         fileSize = ntohl(networkOrder);
     } else {
         status = -1;
     }
 
-    if((n = Rio_readnb(rio, dataBuf, fileSize)) == fileSize) {
+    if(Rio_readnb(rio, dataBuf, fileSize) == fileSize) {
         data = (char*) malloc (sizeof(char)*fileSize);
         memcpy(data, &dataBuf, fileSize);
     } 
@@ -154,23 +162,21 @@ int mcput(rio_t *rio, int connfd) {
         status = -1;
     }
   
-    file = Fopen(fileName, "w");
-        Fwrite(data, sizeof(char), fileSize, file);
-        Fclose(file);
+   myfile = Fopen(fileName, "wb");
+        Fwrite(data, sizeof(char), fileSize, myfile);
+        Fclose(myfile);
         addFile(fileName);
         status = 0;
     
-    //////////////////////////
-    message = (char*) malloc (4);
-    char *bufPosition = message;
+    // Send the status of the operation (success or failure)
+    response = (char*) malloc (4);
+    char *bufPosition = response;
 
     networkOrder = htonl(status);
-    memcpy(bufPosition, &networkOrder, STATUS_SIZE);
-    bufPosition += STATUS_SIZE;
-
-    Rio_writen(connfd, message, 4);
-    free(message);
-
+    memcpy(bufPosition, &networkOrder, 4);
+    bufPosition += 4;
+    
+    Rio_writen(connfd, response, 4);
     return status;
 }
 
@@ -179,7 +185,7 @@ int mcget(rio_t *rio, int connfd) {
     char fileNameBuf[80];
     char fileName[80];
     unsigned int fileSize, networkOrder, status, messageSize;
-    char *data, *message;
+    char *data, *fileData;
     FILE *file;
 
 
@@ -195,8 +201,8 @@ int mcget(rio_t *rio, int connfd) {
         fileSize = 0;
         status = -1; }
     else {
-        
-        file = fopen(fileName, "r");
+        // Open file in binary mode
+        file = fopen(fileName, "rb");
             fseek(file, 0, SEEK_END);
             fileSize = ftell(file);
             rewind(file);
@@ -212,8 +218,8 @@ int mcget(rio_t *rio, int connfd) {
 
     messageSize = STATUS_SIZE + MAX_NUM_BYTES_IN_FILE + fileSize;
 
-    message = (char*) malloc (sizeof(char*)*messageSize);
-    char *bufPosition = message;
+    fileData = (char*) malloc (messageSize);
+    char *bufPosition = fileData;
 
     networkOrder = htonl(status);
     memcpy(bufPosition, &networkOrder, STATUS_SIZE);
@@ -225,10 +231,8 @@ int mcget(rio_t *rio, int connfd) {
 
     memcpy(bufPosition, data, fileSize);
     bufPosition += fileSize;
-    free(data);
-
-    Rio_writen(connfd, message, messageSize);
-
+    
+    Rio_writen(connfd, fileData, messageSize);
     return status;
 }
 
@@ -237,7 +241,7 @@ int mcdel(rio_t *rio, int connfd) {
     char fileNameBuf[80];
     char fileName[80];
     unsigned int networkOrder, status, messageSize;
-    char *message;   
+    char *response;   
     
     if((n = Rio_readnb(rio, fileNameBuf, 80)) == 80) {
         memcpy(&fileName, &fileNameBuf, 80);
@@ -247,27 +251,25 @@ int mcdel(rio_t *rio, int connfd) {
         status = -1;
     }
 
-  
-    if(deleteFile(fileName) == -1) { 
+    if(!fileExists(fileName)) { 
         status = -1; 
         }
     else {
         if(remove(fileName) != 0) { status = -1; }
         else { 
            status = 0; 
-            }
+        }
     }
 
-    messageSize = STATUS_SIZE;
-    message = (char*) malloc (sizeof(char*)*messageSize);
-    char *bufPosition = message;
+    messageSize = 4;
+    response = (char*) malloc(messageSize);
+    char *bufPosition = response;
 
     networkOrder = htonl(status);
-    memcpy(bufPosition, &networkOrder, STATUS_SIZE);
-    bufPosition += STATUS_SIZE;
+    memcpy(bufPosition, &networkOrder, 4);
+    bufPosition += 4;
 
-    Rio_writen(connfd, message, messageSize);
-    free(message);
+    Rio_writen(connfd, response, messageSize);
 
     return status;
 }
@@ -287,8 +289,8 @@ int mclist(rio_t *rio, int connfd) {
     status = 0;
 
     networkOrder = htonl(status);
-    memcpy(bufPosition, &networkOrder, STATUS_SIZE);
-    bufPosition += STATUS_SIZE;
+    memcpy(bufPosition, &networkOrder, 4);
+    bufPosition += 4;
 
     networkOrder = htonl(datalen);
     memcpy(bufPosition, &networkOrder, 4);
@@ -298,12 +300,7 @@ int mclist(rio_t *rio, int connfd) {
     memcpy(bufPosition, fileList[0], datalen);
     bufPosition += datalen;
 
-    for (int i = 0; i < (messageSize); i++){
-        printf("%c | ", message[i]);
-        }
     Rio_writen(connfd, message, messageSize);
-    free(message);
-
     return status;
 }
 
